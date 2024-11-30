48c1461e721db7bc0eaf9f22c8fce7f9f7bd06a3
andre749 <andrecvalera06@gmail.com>
2024-11-30 15:47:26 -0500

    initial commit

---
+++ team2selogra/src/main/java/com/hack2/team2selogra/Team2selograApplication.java
@@ -0,0 +1,13 @@
+package com.hack2.team2selogra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2selograApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2selograApplication.class, args);
+	}
+
+}
---
+++ team2selogra/src/test/java/com/hack2/team2selogra/Team2selograApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack2.team2selogra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2selograApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
