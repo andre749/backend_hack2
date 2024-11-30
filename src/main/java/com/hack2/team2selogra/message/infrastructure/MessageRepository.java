5429889d4ada6c2e8e52c427c4e4cb8db9ca76d7
andre749 <andrecvalera06@gmail.com>
2024-11-30 18:18:34 -0500

    chat communication

--- src/main/java/com/hack2/team2selogra/message/infrastructure/MessageRepository.java
+++ src/main/java/com/hack2/team2selogra/message/infrastructure/MessageRepository.java
@@ -1,13 +1,9 @@
 package com.hack2.team2selogra.message.infrastructure;
 
 import com.hack2.team2selogra.message.domain.Message;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
-import java.util.List;
 import java.util.UUID;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
-    Page<Message> findByChatId(UUID chatId, Pageable pageable);
 }
